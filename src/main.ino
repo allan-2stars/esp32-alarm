4c1f6d7ca949b58f9b1b153bf52e10d398b96aea
Allan Wang <wvllzh@aol.com>
2025-05-18 16:35:23 +1000

    can edit day to repeat on repeat alarm

--- src/main.ino
+++ src/main.ino
@@ -1,4 +1,3 @@
-
 #include <WiFi.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -67,9 +66,18 @@ const unsigned long debounceDelay = 200;
 int selectedAlarmIndex = 0;
 AlarmField selectedField = ALARM_TYPE;
 bool editingField = false;
+int currentRepeatDayIndex = 0;
 
 const char* melodyNames[] = {"Mario", "Birthday", "Xmas"};
 const char* weekDays[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+bool isFieldVisible(AlarmType type, AlarmField field) {
+  if (field == ALARM_DATE_YEAR || field == ALARM_DATE_MONTH || field == ALARM_DATE_DAY)
+    return type == SPECIFIC_DATE;
+  if (field == ALARM_REPEAT_DAYS)
+    return type == REPEATED;
+  return true;
+}
 
 void connectToWiFi() {
   Serial.print("Connecting to WiFi");
@@ -123,70 +131,67 @@ void drawIdleScreen() {
 
 void drawAlarmOverview() {
   display.clearDisplay();
-  display.setTextSize(1);
   display.setCursor(0, 0);
   display.print("Alarms");
-
   for (int i = 0; i < 3; i++) {
     display.setCursor(0, 12 + i * 16);
     display.printf("A%d: %02d:%02d %s", i + 1,
-                   alarms[i].hour, alarms[i].minute,
-                   alarms[i].enabled ? "ON" : "OFF");
+      alarms[i].hour, alarms[i].minute,
+      alarms[i].enabled ? "ON" : "OFF");
   }
   display.display();
 }
 
 void drawAlarmConfig() {
-  Alarm &alarm = alarms[selectedAlarmIndex];
+  Alarm &a = alarms[selectedAlarmIndex];
   display.clearDisplay();
   display.setTextSize(1);
   int y = 0;
 
   display.setCursor(0, y); y += 10;
-  display.print("Config A");
-  display.print(selectedAlarmIndex + 1);
+  display.printf("Config A%d", selectedAlarmIndex + 1);
 
-  display.setCursor(0, y);
-  display.printf("%sType: %s", (selectedField == ALARM_TYPE ? (editingField ? "*" : ">") : " "),
-    (alarm.type == ONE_TIME ? "Once" : alarm.type == SPECIFIC_DATE ? "Date" : "Repeat")); y += 10;
-
-  display.setCursor(0, y);
-  display.printf("%sTime: %02d:%02d", (selectedField == ALARM_TIME_HOUR || selectedField == ALARM_TIME_MIN) ? (editingField ? "*" : ">") : " ", alarm.hour, alarm.minute); y += 10;
-
-  if (alarm.type == SPECIFIC_DATE) {
-    display.setCursor(0, y);
-    display.printf("%sDate: %04d-%02d-%02d", 
-      (selectedField == ALARM_DATE_YEAR || selectedField == ALARM_DATE_MONTH || selectedField == ALARM_DATE_DAY) ? (editingField ? "*" : ">") : " ",
-      alarm.year, alarm.month, alarm.day); y += 10;
+  if (isFieldVisible(a.type, ALARM_TYPE)) {
+    display.setCursor(0, y); y += 10;
+    display.printf("%sType: %s", 
+      selectedField == ALARM_TYPE ? ">" : " ",
+      a.type == ONE_TIME ? "Once" : a.type == SPECIFIC_DATE ? "Date" : "Repeat");
   }
 
-  if (alarm.type == REPEATED) {
-    display.setCursor(0, y);
+  display.setCursor(0, y); y += 10;
+  display.printf("%sTime: %02d:%02d", 
+    (selectedField == ALARM_TIME_HOUR || selectedField == ALARM_TIME_MIN) ? ">" : " ",
+    a.hour, a.minute);
+
+  if (isFieldVisible(a.type, ALARM_DATE_YEAR)) {
+    display.setCursor(0, y); y += 10;
+    display.printf("%sDate: %04d-%02d-%02d",
+      (selectedField == ALARM_DATE_YEAR || selectedField == ALARM_DATE_MONTH || selectedField == ALARM_DATE_DAY) ? (editingField ? "*" : ">") : " ",
+      a.year, a.month, a.day);
+  }
+
+  if (isFieldVisible(a.type, ALARM_REPEAT_DAYS)) {
+    display.setCursor(0, y); y += 10;
     display.print((selectedField == ALARM_REPEAT_DAYS) ? (editingField ? "*Days: " : ">Days: ") : " Days: ");
-    for (int i = 0; i < 7; i++) if (alarm.repeatDays[i]) {
+    for (int i = 0; i < 7; i++) {
+      if (i == currentRepeatDayIndex && selectedField == ALARM_REPEAT_DAYS) display.print("[");
       display.print(weekDays[i][0]);
+      if (a.repeatDays[i]) display.print("*");
+      else display.print(" ");
+      if (i == currentRepeatDayIndex && selectedField == ALARM_REPEAT_DAYS) display.print("]");
       display.print(" ");
     }
-    y += 10;
   }
 
-  display.setCursor(0, y);
-  display.printf("%sEnabled: %s", (selectedField == ALARM_ENABLED) ? (editingField ? "* " : "> ") : " ", alarm.enabled ? "Yes" : "No"); y += 10;
+  display.setCursor(0, y); y += 10;
+  display.printf("%sEnabled: %s", selectedField == ALARM_ENABLED ? ">" : " ", a.enabled ? "Yes" : "No");
 
   display.setCursor(0, y);
-  display.printf("%sMelody: %s", (selectedField == ALARM_MELODY) ? (editingField ? "* " : "> ") : " ", melodyNames[alarm.melody]);
+  display.printf("%sMelody: %s", selectedField == ALARM_MELODY ? ">" : " ", melodyNames[a.melody]);
+
 
   display.display();
 }
-
-bool isFieldVisible(AlarmType type, AlarmField field) {
-  if (field == ALARM_DATE_YEAR || field == ALARM_DATE_MONTH || field == ALARM_DATE_DAY)
-    return type == SPECIFIC_DATE;
-  if (field == ALARM_REPEAT_DAYS)
-    return type == REPEATED;
-  return true;  // all other fields are always visible
-}
-
 
 void setup() {
   Serial.begin(115200);
@@ -196,11 +201,7 @@ void setup() {
   pinMode(SELECT_BUTTON_PIN, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);
   digitalWrite(BUZZER_PIN, LOW);
-
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;);
-  }
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   dht.begin();
   connectToWiFi();
@@ -209,60 +210,53 @@ void setup() {
 void loop() {
   unsigned long now = millis();
 
-  if (digitalRead(MODE_BUTTON_PIN) == LOW && now - lastModePress > 200) {
+  if (digitalRead(MODE_BUTTON_PIN) == LOW && now - lastModePress > debounceDelay) {
     lastModePress = now;
     if (uiState == ALARM_OVERVIEW) {
       uiState = ALARM_CONFIG;
       selectedField = ALARM_TYPE;
       editingField = false;
     } else if (uiState == ALARM_CONFIG) {
-      if (editingField && selectedField == ALARM_REPEAT_DAYS) {
-        for (int i = 0; i < 7; i++) alarms[selectedAlarmIndex].repeatDays[i] = false;
-        editingField = false;
-      } else if (selectedField == ALARM_SAVE_EXIT) {
-        uiState = ALARM_OVERVIEW;
-        selectedField = ALARM_TYPE;
-        editingField = false;
-      } else {
-        do {
-          selectedField = (AlarmField)((selectedField + 1) % 10);
-        } while (!isFieldVisible(alarms[selectedAlarmIndex].type, selectedField));
-
-        editingField = false;
-      }
+      do {
+        selectedField = (AlarmField)((selectedField + 1) % 10);
+      } while (!isFieldVisible(alarms[selectedAlarmIndex].type, selectedField));
+      editingField = false;
     } else {
       uiState = (UIState)((uiState + 1) % 3);
     }
     lastInteractionTime = now;
   }
 
-  if (digitalRead(ADJUST_BUTTON_PIN) == LOW && now - lastAdjustPress > 200) {
+  if (digitalRead(ADJUST_BUTTON_PIN) == LOW && now - lastAdjustPress > debounceDelay) {
     lastAdjustPress = now;
-    Alarm &alarm = alarms[selectedAlarmIndex];
+    Alarm &a = alarms[selectedAlarmIndex];
     switch (selectedField) {
-      case ALARM_TYPE: alarm.type = (AlarmType)((alarm.type + 1) % 3); break;
-      case ALARM_TIME_HOUR: alarm.hour = (alarm.hour + 1) % 24; break;
-      case ALARM_TIME_MIN: alarm.minute = (alarm.minute + 1) % 60; break;
-      case ALARM_DATE_YEAR: alarm.year = (alarm.year < 2035) ? alarm.year + 1 : 2025; break;
-      case ALARM_DATE_MONTH: alarm.month = (alarm.month % 12) + 1; break;
-      case ALARM_DATE_DAY: alarm.day = (alarm.day % 31) + 1; break;
-      case ALARM_REPEAT_DAYS:
-        for (int i = 0; i < 7; i++) alarm.repeatDays[i] = !alarm.repeatDays[i];
-        break;
-      case ALARM_ENABLED: alarm.enabled = !alarm.enabled; break;
-      case ALARM_MELODY: alarm.melody = (alarm.melody + 1) % 3; break;
+      case ALARM_TYPE: a.type = (AlarmType)((a.type + 1) % 3); break;
+      case ALARM_TIME_HOUR: a.hour = (a.hour + 1) % 24; break;
+      case ALARM_TIME_MIN: a.minute = (a.minute + 1) % 60; break;
+      case ALARM_DATE_YEAR: a.year = (a.year < 2035) ? a.year + 1 : 2025; break;
+      case ALARM_DATE_MONTH: a.month = (a.month % 12) + 1; break;
+      case ALARM_DATE_DAY: a.day = (a.day % 31) + 1; break;
+      case ALARM_REPEAT_DAYS: currentRepeatDayIndex = (currentRepeatDayIndex + 1) % 7; break;
+      case ALARM_ENABLED: a.enabled = !a.enabled; break;
+      case ALARM_MELODY: a.melody = (a.melody + 1) % 3; break;
       default: break;
     }
     lastInteractionTime = now;
   }
 
-  if (digitalRead(SELECT_BUTTON_PIN) == LOW && now - lastSelectPress > 200) {
+  if (digitalRead(SELECT_BUTTON_PIN) == LOW && now - lastSelectPress > debounceDelay) {
     lastSelectPress = now;
     lastInteractionTime = now;
     if (uiState == ALARM_OVERVIEW) {
       selectedAlarmIndex = (selectedAlarmIndex + 1) % 3;
     } else if (uiState == ALARM_CONFIG) {
-      editingField = !editingField;
+      if (selectedField == ALARM_REPEAT_DAYS) {
+        alarms[selectedAlarmIndex].repeatDays[currentRepeatDayIndex] = 
+          !alarms[selectedAlarmIndex].repeatDays[currentRepeatDayIndex];
+      } else {
+        editingField = !editingField;
+      }
     }
   }
 
