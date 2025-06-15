a577d56a75f21efe6e8665b291b3908d19e54a92
Allan Wang <wvllzh@aol.com>
2025-06-15 13:21:07 +1000

    add define for servo and PIR pin in config.h

--- include/config.h
+++ include/config.h
@@ -25,7 +25,11 @@
 #define TOUCH_WAKE_PIN T3 // GPIO15 integrated
 //#define SHOCK_SENSOR_PIN 4
 #define SHOCK_SENSOR_PIN 0 // Wokwi
-#define SERVO_PIN 13 // for open the door
+#define SERVO_PIN 13
+#define PIR_PIN 5
+#define SERVO_HOURLY_ANGLE 90
+#define SERVO_MOTION_ANGLE 15
+
 
 #define SCREEN_CONTRAST_PIN 21
 #define MODE_BUTTON_PIN     33
